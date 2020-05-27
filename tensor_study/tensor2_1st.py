1f64ca92c895a9af120f7159e72d32fa79d228b2
develomona <develomona@gmail.com>
2020-05-27 23:03:18 +0900

    Folder created, 2nd file upload

---
+++ tensor_study/tensor2_1st.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+rand_uniform = tf.random.uniform([4], 0, 1)
+rand_normal = tf.random.normal([4], 0, 1)
+print(rand_uniform, rand_normal)
+print(rand_uniform)
---
+++ tensor_study/tensor2_2nd.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import math
+
+def sigmoid(x):
+    return 1 / (1+math.exp(-x))
+
+x = 1
+y = 0
+w = tf.random.normal([1], 0, 1)
+b = tf.random.normal([1], 0, 1) # bias
+
+for i in range(1000):
+    output = sigmoid(x*w)
+    error = y - output
+    w = w + x * 0.1 * error
+    b = b + 1 * 0.1 * error
+
+    if i % 100 == 99:
+        print(i, error, output)
